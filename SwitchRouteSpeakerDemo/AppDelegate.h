f641351c828dbe8c705d603d2631f9808ae58fd4
Tony <hanningkong@gmail.com>
2015-01-05 23:28:09 +0800

    Initial Commit

---
+++ SwitchRouteSpeakerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwitchRouteSpeakerDemo
+//
+//  Created by Tony on 1/5/15.
+//  Copyright (c) 2015 Didi.Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
